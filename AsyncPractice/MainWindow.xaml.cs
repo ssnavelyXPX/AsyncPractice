16f99051ccd6f7e13fd76c78959cd76317be8103
Seth Snavely <ssnavely@xpanxion.com>
2018-06-21 19:07:20 -0500

    Initial layout of AsyncPractice app

--- AsyncPractice/MainWindow.xaml.cs
+++ AsyncPractice/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace AsyncPractice
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
